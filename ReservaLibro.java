015bbf8d35b9895cd80348a8161ff7de25f54402
Madonias88 <chester2223@gmail.com>
2025-05-10 13:08:17 -0600

    Agregado archivo ReservaLibro.java

---
+++ ReservaLibro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reservalibro;
+
+/**
+ *
+ * @author Mario
+ */
+public class ReservaLibro {
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese libro a reservar");
+        System.out.println("Reserva creada");
+    }
+}
